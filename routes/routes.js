6f6a12565c9dc769bba7067831838f4b4f9986c0
Hatemii <hatem.xhezairi96@gmail.com>
2022-07-01 16:47:41 +0200

    more refactors

--- routes/routes.js
+++ routes/routes.js
@@ -1,64 +1,67 @@
 const express = require("express");
 const repoModel = require("../models/repo_model");
-const bodyParser = require('body-parser')
-const axios = require('axios').default;
+const bodyParser = require("body-parser");
+const axios = require("axios").default;
 const app = express();
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  app.get('/user/signin/callback', (req, res) => {
-    const { query } = req;
-    const { code } = query;
+  res.send("Server is running...");
+});
 
-    if (!code) {
-      return res.send({
-        success: false,
-        message: "no code"
-      })
-    };
+app.get("/user/signin/callback", (req, res) => {
+  const { query } = req;
+  const { code } = query;
 
-    axios({
-      method: 'post',
-      url: 'https://github.com/login/oauth/access_token',
-      data: {
-        client_id: "aa00582765f9040754a5",
-        client_secret: "d3993753b6011a136b1eea9b8c12a5b11e3ccda9",
-        code: code
-      },
-      headers: { 'Content-Type': 'application/json' }
-    }).then(function (result) {
-      result.data ? res.redirect('http://localhost:8080/user_profile') : res.redirect('/')  
-    })
-  })
-})
+  if (!code) {
+    return res.send({
+      success: false,
+      message: "no code",
+    });
+  }
+
+  axios({
+    method: "post",
+    url: "https://github.com/login/oauth/access_token",
+    data: {
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET,
+      code: code,
+    },
+    headers: { "Content-Type": "application/json" },
+  }).then(function (result) {
+    result.data
+      ? res.redirect("http://localhost:8081/user_profile")
+      : res.redirect("/");
+  });
+});
 
 app.get("/user_profile", (req, res) => {
-  console.log("User Profile")
   axios({
-      method: 'get',
-      url: 'https://api.github.com/users/Hatemii/repos',
-      headers: { 'Content-Type': 'application/json' }
+    method: "get",
+    url: "https://api.github.com/users/Hatemii/repos",
+    headers: { "Content-Type": "application/json" },
   }).then(function (result) {
+    res.send(result.data);
     const myObj = result.data.map((record) => {
       return {
         name: record.name,
         url: record.html_url,
         description: record.description,
-        user: record.owner.id
-      }
-    })
-    // repoModel.insertMany(myObj)
-  })
-  // res.sendFile(__dirname + "/views/user_profile.html");
-})
+        user: record.owner.id,
+      };
+    });
+    repoModel.insertMany(myObj);
+  });
+});
 
 app.get("/public_repositories", async (request, response) => {
-  // const repositories = await repoModel.find();
-  // try {
-  //   response.send(repositories);
-  // } catch (error) {
-  //   response.send(error);
-  // }
+  const repositories = await repoModel.find();
+  try {
+    response.send(repositories);
+  } catch (error) {
+    response.send(error);
+  }
 });
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,74 +1,14 @@
 const express = require("express");
 const MongoDb = require("./db/db_config");
+const Router = require("./routes/routes");
 const cors = require("cors");
-const axios = require("axios").default;
-const repoModel = require("./models/repo_model");
 const app = express();
 
 app.use(MongoDb);
 app.use(cors({ origin: "*" }));
 
-// app.use("/", Router)
-// app.use("/user_profile", Router)
-// app.use("/public_repositories", Router)
-
-app.get("/", (req, res) => {
-  res.send("Server is running...");
-});
-
-app.get("/user/signin/callback", (req, res) => {
-  const { query } = req;
-  const { code } = query;
-
-  if (!code) {
-    return res.send({
-      success: false,
-      message: "no code",
-    });
-  }
-
-  axios({
-    method: "post",
-    url: "https://github.com/login/oauth/access_token",
-    data: {
-      client_id: process.env.CLIENT_ID,
-      client_secret: process.env.CLIENT_SECRET,
-      code: code,
-    },
-    headers: { "Content-Type": "application/json" },
-  }).then(function (result) {
-    result.data
-      ? res.redirect("http://localhost:8081/user_profile")
-      : res.redirect("/");
-  });
-});
-
-app.get("/user_profile", (req, res) => {
-  axios({
-    method: "get",
-    url: "https://api.github.com/users/Hatemii/repos",
-    headers: { "Content-Type": "application/json" },
-  }).then(function (result) {
-    res.send(result.data)
-    const myObj = result.data.map((record) => {
-      return {
-        name: record.name,
-        url: record.html_url,
-        description: record.description,
-        user: record.owner.id,
-      };
-    });
-    repoModel.insertMany(myObj);
-  });
-});
-
-app.get("/public_repositories", async (request, response) => {
-  const repositories = await repoModel.find();
-  try {
-    response.send(repositories);
-  } catch (error) {
-    response.send(error);
-  }
-});
+app.use("/", Router);
+app.use("/user_profile", Router);
+app.use("/public_repositories", Router);
 
 app.listen(8080, () => console.log("server is running on port 8080"));
